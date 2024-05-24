8c8d1f00063e239bcbc8e27b6ce8b217604139a6
Sergiodmj <sergiomazutti@gmail.com>
2024-05-23 21:10:56 -0300

    deleted:    pages/components/Layout.js 	modified:   pages/components/Navbar.js

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -1,38 +1,27 @@
-import Link from "next/link"
-import Image from "next/image"
+import Link from "next/link";
+import Image from "next/image";
 
-import styles from '../../styles/navbar.module.css'
+import styles from "../../styles/navbar.module.css";
 
 export default function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>
+        <Image src={"/Images/logo.png"} width="70" height="70" alt="Logo" />
+      </div>
 
-    return (
-        <nav className={styles.navbar}>
-            <div className={styles.logo}>
-                <Image
-                    src={"/Images/logo.png"}
-                    width="70"
-                    height="70"
-                    alt="Logo"
-                />
-
-            </div>
-
-            <ul className={styles.link_items}>
-                <li>
-                    <Link legacyBehavior href="/">
-                        <a>Cadastro</a>
-                    </Link>
-                </li>
-                <li>
-                    <Link legacyBehavior href="/about">
-                        <a>Sobre</a>
-                    </Link>
-                </li>
-            </ul>
-
-        </nav>
-
-
-    )
-
+      <ul className={styles.link_items}>
+        <li>
+          <Link legacyBehavior href="/">
+            <a>Cadastro</a>
+          </Link>
+        </li>
+        <li>
+          <Link legacyBehavior href="/about">
+            <a>Cliente</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
