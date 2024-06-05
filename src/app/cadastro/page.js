f98a4f23f25d43bb69be2050e755d50bc56ad490
Sérgio Mazutti <101757048+Sergiodmj@users.noreply.github.com>
2024-06-05 16:23:46 -0300

    modified:   package-lock.json 	modified:   package.json	modified:   src/app/cadastro/page.js 	new file:   tsconfig.json

--- src/app/cadastro/page.js
+++ src/app/cadastro/page.js
@@ -2,56 +2,34 @@ import style from "../../../styles/formulario.module.css"
 
 export default function Cadastro() {
 
-    function createPost(cliente) {
-        fetch('http://n8npay.zapto.org:9099/api-beck/insert.php', {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json',
-            },
-        })
-    }
+    async function salvar() {
+        'use server'
 
-    const submit = (e) => {
-        e.preventDefault()
-        handleSubmit(cliente)
+        // event.preventDefault()
+
+        // if ((!nome) || (!sobrenome) || (!email)) {
+        //     return
+        // }
+
+        await fetch('http://n8npay.zapto.org:9099/api-beck/insert.php', {
+            method: 'POST',
+            body: JSON.stringify({
+                nome, sobrenome, email
+            })
+        })
+
     }
 
     return (
-        <>
-            <div className={style.container}>
-                <form className={style.form} action="Cadastro">
-                    <div className={style.input_field}>
-                        <input
-                            type="text"
-                            name="text"
-                            id="nome"
-                        />
-                        <label for="username">Nome</label>
-                    </div>
-                    <div className={style.input_field}>
-                        <input
-                            type="text"
-                            name="text"
-                            id="sobrenome"
-                        />
-                        <label for="username">Sobrensome</label>
-                    </div>
-                    <div className={style.input_field}>
-                        <input
-                            type="text"
-                            name="text"
-                            id="email"
-                        />
-                        <label for="username">E-mail</label>
-                    </div>
 
-                    <div className={style.btn_container}>
-                        <button className={style.btn}>Salvar</button>
+        <div>
+            <form action={salvar}>
+                <input name="nome" placeholder="Nome" />
+                <input name="sobrenome" placeholder="sobrenome" />
+                <input name="email" placeholder="sobrenome" />
+                <button >Salvar</button>
+            </form>
+        </div>
 
-                    </div>
-                </form>
-            </div>
-
-        </>
     )
 }
