c99b8e82fd749aeddd98909fe105944c3854d314
Sérgio Mazutti <101757048+Sergiodmj@users.noreply.github.com>
2024-06-18 15:32:55 -0300

    modified:   cadastro/src/app/pages/cadastroClientes/page.tsx 	modified:   cadastro/src/app/pages/tabelaCliente/page.tsx

--- cadastro/src/app/pages/cadastroClientes/page.tsx
+++ cadastro/src/app/pages/cadastroClientes/page.tsx
@@ -1,47 +1,35 @@
-import { revalidateTag } from "next/cache"
-import Cliente from "../tabelaCliente/page"
+import { revalidateTag } from "next/cache";
+import Cliente from "../tabelaCliente/page";
 
 export default function CadastroClientes() {
-  
-    // Recebe os dados do dormulario
-    async function createCliente(form: FormData) {
-        'use server'
+  // Recebe os dados do dormulario
+  async function createCliente(form: FormData) {
+    "use server";
 
-        //Converte os dados em um objeto do next
-         const data = Object.fromEntries(form);
+    //Converte os dados em um objeto do next
+    const data = Object.fromEntries(form);
 
-        //Faz uma validação, se o objeto não conter dados, ele simplesmente retorna e nao salva nada
-        if (!data) {
-            return
-        }
+    // Faz uma requisição POST pasa a rota
+    await fetch("http://n8npay.zapto.org:9099/api-beck/insert.php", {
+      //Informa o metodo que esta usando
+      method: "POST",
+      body: JSON.stringify(data), //encaminha o objeto para a rota como JSON.
+    });
 
-        // Faz uma requisição POST pasa a rota
-        await fetch('http://n8npay.zapto.org:9099/api-beck/insert.php', {
-        //Informa o metodo que esta usando
-         method: 'POST',
-         body: JSON.stringify(data)    //encaminha o objeto para a rota como JSON.
-         })
-        
-        // console.log(data);       
+    console.log(data);
 
-
-        //Indica qual o componente deve ser atualizado depois da execução da função
-        revalidateTag('tabela-cliente')
-
+    //Indica qual o componente deve ser atualizado depois da execução da função
+    revalidateTag("tabela-cliente");
+  }
+  return (
+    <>
+      <form action={createCliente} method="POST">
+        <input type="text" name="nome" placeholder="Nome" required />
+        <input type="text" name="sobrenome" placeholder="Sobrenome" required />
+        <input type="email" name="email" placeholder="E-mail" required />
+        <button type="submit">Salvar</button>
+      </form>
+      <Cliente />
+    </>
+  );
 }
-
-    return (
-        <>
-            <form action={createCliente} method="POST">
-                <input type="text" name="nome" placeholder="Nome" />
-                <input type="text" name="sobrenome" placeholder="Sobrenome" />
-                <input type="text" name="email" placeholder="E-mail" />
-                <button type="submit">Salvar</button>
-            </form>
-            <Cliente/>
-        </>
-    )
-}
-
-
-//http://n8npay.zapto.org:9099/api-beck/insert.php
--- cadastro/src/app/pages/tabelaCliente/page.tsx
+++ cadastro/src/app/pages/tabelaCliente/page.tsx
@@ -1,83 +1,85 @@
 import { revalidateTag } from "next/cache";
+import Link from "next/link";
 
 export default async function Cliente() {
-    'use server'
-    const res = await fetch('http://n8npay.zapto.org:9099/api-beck/', {
+  "use server";
+  const res = await fetch("http://n8npay.zapto.org:9099/api-beck/", {
+    // força o navegador a não guardar o cash, ele vai fazer uma nova requisição cada vez que for executado
+    cache: "no-cache",
 
-        // força o navegador a não guardar o cash, ele vai fazer uma nova requisição cada vez que for executado
-        cache: 'no-cache',
+    //método que faz atualizar o componente assim que alterado algum dado, sem precisar atualizar a página.
+    next: {
+      tags: ["tabela-cliente"],
+    },
 
-        //método que faz atualizar o componente assim que alterado algum dado, sem precisar atualizar a página.
-        next: {
-            tags: ['tabela-cliente']
-        }
+    // caso queira limitar o tempo em que o navegador guarda o cash
+    // next: {
+    //     revalidate: 60
+    // }
+  });
 
-        // caso queira limitar o tempo em que o navegador guarda o cash
-        // next: {
-        //     revalidate: 60
-        // }
+  const data = await res.json();
 
-    })
+  async function deleteCliente(form: FormData) {
+    "use server";
 
-    async function DeleteData({id}) {
-     'use server'
-    console.log(typeof id)
-    try {
-        const response = await fetch(`${"http://n8npay.zapto.org:9099/api-beck/delete.php/"}${id}`, { // Faz uma requisição DELETE para o URL fornecido com o ID específico.
-            method: 'GET', // Define o método da requisição como DELETE.
-        });
+    //Converte os dados em um objeto do next
+    const data = Object.fromEntries(form);
 
-        if (!response.ok) { // Verifica se a resposta HTTP indica um sucesso (código 2xx).
-            throw new Error(`Failed to delete data: ${response.statusText}`); // Se a resposta não for bem-sucedida, lança um erro com a mensagem da resposta.
-        }
-
-        console.log('Data deleted successfully'); // Se a exclusão for bem-sucedida, exibe uma mensagem de sucesso.
-        } catch (error) { // Captura e trata quaisquer erros que ocorram durante o processo.
-        console.error('Error deleting data:', error); // Se ocorrer um erro, exibe uma mensagem de erro com detalhes.
-        }
-        
-     revalidateTag('tabela-cliente')
-         
+    //Faz uma validação, se o objeto não conter dados, ele simplesmente retorna e nao salva nada
+    if (!data) {
+      return;
     }
-    
-    const data = await res.json();
-    
-    return (
-        <>
-            
-            <table>
-                <tbody>
-                <tr>
-                    <th>ID</th>
-                    <th>NOME</th>
-                    <th>SOBRENOME</th>
-                    <th>E-MAIL</th>
-                    <th>DOCUMENTO</th>
-                    <th></th>
-                </tr>
-                    {data.map((data: any) => {
-                        return (
-                            <tr key={data.id}>
-                                <td>{ data.id }</td>
-                                <td>{ data.first_name }</td>
-                                <td>{ data.last_name }</td>
-                                <td>{ data.email }</td>
-                                <td>{ data.document }</td>
-                                <td><button>Alterar</button></td>
-                                <td>
-                                    <form action={DeleteData(data)}>
-                                        <button type="submit">
-                                            <span>Delete</span>
-                                        </button>
-                                    </form>
-                                </td>
-                                
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
-        </>
-    )
-}
 
+    // Faz uma requisição POST pasa a rota
+    await fetch("http://n8npay.zapto.org:9099/api-beck/delete.php", {
+      //Informa o metodo que esta usando
+      method: "POST",
+      body: JSON.stringify(data), //encaminha o ID do objeto para a rota como JSON.
+    });
+
+    console.log(data);
+
+    //Indica qual o componente deve ser atualizado depois da execução da função
+    revalidateTag("tabela-cliente");
+  }
+
+  return (
+    <>
+      <table>
+        <tbody>
+          <tr>
+            <th>ID</th>
+            <th>NOME</th>
+            <th>SOBRENOME</th>
+            <th>E-MAIL</th>
+            <th>DOCUMENTO</th>
+            <th></th>
+          </tr>
+          {data.map((data: any) => {
+            return (
+              <tr key={data.id}>
+                <td>{data.id}</td>
+                <td>{data.first_name}</td>
+                <td>{data.last_name}</td>
+                <td>{data.email}</td>
+                <td>{data.document}</td>
+                <td>
+                  <Link href="/pages/cadastroClientes">
+                    <button>Alterar</button>
+                  </Link>
+                </td>
+                <td>
+                  <form action={deleteCliente} method="POST">
+                    <input type="hidden" name="id" value={data.id} />
+                    <button type="submit">Excluir</button>
+                  </form>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+}
